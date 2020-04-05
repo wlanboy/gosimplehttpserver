fb8b60ef2145f2f49a56545b2d8b16ad9b5142d3
wlanboy <wlanboy@users.noreply.github.com>
2020-04-05 11:49:48 +0200

    Create ip.go

---
+++ basic/ip.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func filterPort(data string) string {
+	if strings.Contains(data, ":") {
+		data = strings.Split(data, ":")[0]
+	}
+	return data
+}
+
+func getip(r *http.Request) string {
+	ip := r.Header.Get("Http-Client.Ip")
+	if ip == "" {
+		ip = r.Header.Get("X-Forwarded-For")
+	}
+	if ip == "" {
+		ip = filterPort(r.RemoteAddr)
+	}
+	return ip
+}
+
+func iphandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("%s\t%s", r.Method, r.RequestURI)
+	fmt.Fprintf(w, "%s", filterPort(getip(r)))
+}
+
+func hosthandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("%s\t%s", r.Method, r.RequestURI)
+	fmt.Fprintf(w, "%s", filterPort(r.Host))
+}
+
+func agenthandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("%s\t%s", r.Method, r.RequestURI)
+	fmt.Fprintf(w, "%s", r.UserAgent())
+}
+
+func headershandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("%s\t%s", r.Method, r.RequestURI)
+	for name, value := range r.Header {
+		fmt.Fprintf(w, "%v: %v\n", name, value)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", iphandler)
+	http.HandleFunc("/ip", iphandler)
+	http.HandleFunc("/host", hosthandler)
+	http.HandleFunc("/agent", agenthandler)
+	http.HandleFunc("/header", headershandler)
+
+	log.Println("start web server")
+	log.Fatal(http.ListenAndServe(":7000", nil))
+	log.Println("stop web server")
+}
